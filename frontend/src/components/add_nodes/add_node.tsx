be82737e88858f5136584c870f09d082b513173a
RoJoSaVs <ronnyjosv259@outlook.es>
2021-11-05 01:06:47 -0600

    Add ONG method working and tested with Backend

--- frontend/src/components/add_nodes/add_node.tsx
+++ frontend/src/components/add_nodes/add_node.tsx
@@ -12,6 +12,12 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+
+
 import {Dialog,DialogContent,DialogActions,DialogContentText,CircularProgress} from '@material-ui/core';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
@@ -21,6 +27,51 @@ interface Employee {
   job: string;
   id: string;
 };
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container1: {
+      // display: 'flex',
+      // flexWrap: 'wrap',
+      width: 300,
+      margin: `${theme.spacing(0)} left`
+      // margin: `auto`
+    },
+    container2: {
+      // display: 'flex',
+      flexWrap: 'wrap',
+      width: 300,
+      margin: `${theme.spacing(1)} center`
+      // margin: `auto`
+    },
+    container3: {
+      // display: 'flex',
+      // flexWrap: 'wrap',
+      width: 300,
+      margin: `${theme.spacing(2)} right`
+      // margin: `auto`
+    },
+
+    loginBtn: {
+      marginTop: theme.spacing(2),
+      flexGrow: 1
+    },
+    header: {
+      textAlign: 'center',
+      background: '#212121',
+      color: '#fff'
+    },
+    card: {
+      marginTop: theme.spacing(5)
+    },
+    table: {
+    minWidth: 700,
+  },
+  })
+);
+
+
 
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
@@ -44,11 +95,6 @@ const StyledTableRow = withStyles((theme: Theme) =>
   }),
 )(TableRow);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
 
 interface IPost {
   "p_name": string,
@@ -76,7 +122,13 @@ const App = () => {
   const [error, setError]: [string, (error: string) => void] = React.useState(
     ''
   );
+  // ONG Values
+  const [ong_name, setOngName] = React.useState('')
+  const [ong_country, setOngCountry] = React.useState('')
 
+  // Project Values
+
+  // Volunteer Values
   const [volunteer, setProject] = React.useState('')
 
   const cancelToken = axios.CancelToken; //create cancel token
@@ -86,10 +138,16 @@ const App = () => {
   ] = React.useState(cancelToken.source());
 
 
-  const addNode = () =>{
+  const addOng = () =>{
     axios
-      .post('https://Proyecto-2-BDA.ronnysantamaria.repl.co/api/add', "Hola")
-  }
+      .post('https://Proyecto-2-BDA.ronnysantamaria.repl.co/api/add', { "type": "ong", "name": ong_name, "country": ong_country })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      }
 
 
   const getData = () => {
@@ -110,26 +168,55 @@ const App = () => {
 
 
   return (
-    <TableContainer component={Paper}>
-    <form>
-        <div className="container">
-          <div className="form-group">
-          <TextField
-              fullWidth
-              id="text"
-              type="text"
-              label="Volunteer"
-              placeholder="Volunteer"
-              margin="normal"
-              onChange={event => setProject(event.target.value)}
-            />
-            <Button className="btn btn-primary" color="primary" onClick={addNode}>
-              Submit
-            </Button>
+    <form className={classes.container1} noValidate autoComplete="off">
+      <Card className={classes.card}>
+        <CardHeader className={classes.header} title="ONG" />
+        <CardContent>
+          <div>
+            <TextField fullWidth id="ong_name" type="text" label="Name" placeholder="Name" onChange={event => setOngName(event.target.value)}/>
+            <TextField fullWidth id="ong_country" type="text" label="Country" placeholder="Country" onChange={event => setOngCountry(event.target.value)}/>
           </div>
-        </div>
-      </form>
-    </TableContainer>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="large" color="primary" onClick={addOng}>
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+      <form className={classes.container2} noValidate autoComplete="off">
+      <Card className={classes.card}>
+        <CardHeader className={classes.header} title="Project" />
+        <CardContent>
+          <div>
+            <TextField id="project_name" type="text" label="Name" placeholder="Name" margin="normal" onChange={event => setProject(event.target.value)}/>
+            <TextField id="project_country" type="text" label="Country" placeholder="Country" margin="normal" onChange={event => setProject(event.target.value)}/>
+            <TextField id="project_reach" type="text" label="Reach" placeholder="Reach" margin="normal" onChange={event => setProject(event.target.value)}/>
+            <TextField id="project_duration" type="text" label="Duration" placeholder="Duration" margin="normal" onChange={event => setProject(event.target.value)}/>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="large" color="primary">
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+      <Card className={classes.card}>
+        <CardHeader className={classes.header} title="Volunteer" />
+        <CardContent>
+          <div>
+            <TextField fullWidth id="volunteer_name" type="text" label="Name" placeholder="Name"/>
+            <TextField fullWidth id="volunteer_country" type="text" label="Country" placeholder="Country"/>
+            <TextField fullWidth id="volunteer_age" type="text" label="Age" placeholder="Age"/>
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" size="large" color="primary">
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+    </form>
   );
 };
 
